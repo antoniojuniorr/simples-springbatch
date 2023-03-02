874e5a62cecc9c8d53fafb59a0827599b273fb91
Antonio Junior <antonioeloijunior@gmail.com>
2023-03-01 22:08:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Batch Restart
+
+Projeto para demonstrar como funciona o Spring Batch de forma bem básica.
+
+O projeto possui um job que lê uma lista de um a 100 e imprime os valores lidos.
+
---
+++ src/main/java/br/com/antonio/springbatchsimples/SpringbatchSimplesApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonio.springbatchsimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchSimplesApplication.class, args);
+	}
+
+}
